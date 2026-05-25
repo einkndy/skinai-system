81ce2796331feb9db3b727bfded59a00b111ec27
einkndy <einstenkondoy@gmail.com>
2026-05-25 21:44:23 +0800

    Fix production CORS

--- skin-ai-backend/main.py
+++ skin-ai-backend/main.py
@@ -44,7 +44,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://localhost:5173",
+        "https://skinai-system.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
